6022cec4abecb01a0b7933f31b253e5af375f04a
Nnabuz67 <nnabuikeokafor24@gmail.com>
2022-08-23 16:20:52 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+ // This is my first javascript code!
+ //let name = "MOSH"
+  //console.log(name);
+
+  //const interestRate = 0.3;
+  //interestRate = 1;
+  //console.log(interestRate)
+
+ // let name = 'Mosh'; //string literal
+  //let age = '30'; //number literal
+ // let isApproved = true; //Boolean Literal
+ // let firstName = undefined;
+  //let selectedColor =null;
+
+
+  let person = {
+       name: 'Mosh',
+       age: 30
+  };
+
+  console.log(person);
---
+++ index.py
@@ -0,0 +1,31 @@
+# class declaration
+# class Student:
+      #Constructor
+  #  def __init__(self, fname, lname, email, level):
+   #     self.firstname = fname
+    #    self.lastname = lname
+     #   self.email = f'{lastname}{firstname}@gmail.com'.lower
+      #  self.level = level
+
+   # def Inform(self):
+    #    print(self.firstname, self.lastname, self.email, self.level)
+
+# x = Student("Okafor", "Nnabuike", 200)
+# x.Inform()
+
+#mazi = Student("Peter", "Johnbull",  500)
+#mazi.Inform()
+
+
+#instance variable
+class Student:
+    pass
+
+okafor = Student()
+
+okafor.name = 'okafor'
+okafor.laptop = 'hp'
+okafor.jacket = 'black'
+okafor.phone = 'Nokia'
+
+print(okafor)
